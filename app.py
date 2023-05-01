16ea7c646ec87758892cb4d9ec6cdb8d867edd51
Ankur Kaushal <97340597+Avenster@users.noreply.github.com>
2023-05-02 01:00:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,28 +29,21 @@
 # if __name__ == '__main__':
 #     app.run()
 
-
 import pickle
-from flask import Flask, request, jsonify
 import pandas as pd
-import numpy as np
-
-app = Flask(__name__)
+import streamlit as st
 
 with open('grid.pkl', 'rb') as file:
     model = pickle.load(file)
 
-@app.route('/', methods=['GET'])
-def predict():
-    text=request.args.get('text')
-    s = pd.Series([text])
-    y_pred = model.predict(s)
-    print(y_pred)
-    output={
-        'result': y_pred.tolist()[0] # convert numpy array to a list
-    }
-    print(output)
-    return jsonify(output)
+def app():
+    st.title("Hate Speech Prediction App")
+    text = st.text_input("Enter your text here")
+    if st.button("Predict"):
+        s = pd.Series([text])
+        y_pred = model.predict(s)
+        st.write("Prediction:", y_pred[0])
 
 if __name__ == '__main__':
-    app.run()
+    app()
+
